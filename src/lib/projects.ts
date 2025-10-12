1938790921873f6527113dcc0ee7feca984c0435
chriscocose <chriscocose@icloud.com>
2025-10-13 07:00:57 +0900

    updated miami entry ui/ux

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -56,6 +56,19 @@ export const projects: Project[] = [
     mediaType: "image",
     mediaSrc: "/videos/lj.mp4",
   },
+  {
+    id: "miami",
+    slug: "miami-case",
+    title: "Miami Dolphins - UI/UX Case Study",
+    year: 2025,
+    roles: ["Design", "VFX", "UI/UX"],
+    tags: ["UI/UX"],
+    summary:
+      "I designed a motion-driven player-page concept for the Miami Dolphins that merges broadcast-level visual storytelling with user-centric interaction design. Using After Effects, I utilized VFX and kinetic typography inspired by team broadcast packages — blending color, motion rhythm, and type hierarchy to capture the brand’s athletic energy. I then brought those visuals into Figma, translating the motion language into a responsive UI/UX system featuring the animations and a streamlined web/mobile layout. The result is a cohesive fan experience where motion enhances usability — not just aesthetics — and every transition reflects the team’s speed and personality. This project bridges my background in VFX, typography, and front-end design, demonstrating how cinematic motion principles can elevate interactive design and storytelling in digital sports experiences. Here is a link to the case study: https://www.figma.com/proto/8CSnt28vrtgRIlJL8LWLbu/Miami-Dolphins---Player-Redesign?node-id=2-2378&p=f&t=VCMSe6v5c4AmTswv-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=2%3A2378 ",
+    cover: "/covers/miami.jpg",
+    mediaType: "image",
+    mediaSrc: "/videos/miami.mp4",
+  },
 
 
 ];
